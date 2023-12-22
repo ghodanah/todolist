e5ca1ead5e5f145b6afe0a87f337929459ffb62f
ghodanah <hhh1529@gmail.com>
2023-12-22 15:00:54 +0900

    todo 작성화면 꾸미기

--- lib/create_screen.dart
+++ lib/create_screen.dart
@@ -8,10 +8,25 @@ class CreateScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo 작성'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.done),
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            hintStyle: TextStyle(color: Colors.grey[800]),
+            hintText: '할일을 입력하세요.',
+            filled: true,
+            fillColor: Colors.lightGreenAccent,
+          ),
         ),
       ),
     );
