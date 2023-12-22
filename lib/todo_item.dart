076cc419fbf2e1acb7a15bdd9ea8d51d93c05386
ghodanah <hhh1529@gmail.com>
2023-12-22 16:34:16 +0900

    날짜 표시 변경(DateFormat 활용)

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:todolist/todo.dart';
 
 class TodoItem extends StatelessWidget {
@@ -18,17 +19,20 @@ class TodoItem extends StatelessWidget {
         onTap(todo);
       },
       leading: todo.isDone
-          ? Icon(
+          ? const Icon(
               Icons.check_circle,
               color: Colors.green,
             )
-          : Icon(Icons.check_circle_outline),
+          : const Icon(Icons.check_circle_outline),
       title: Text(
         todo.title,
         style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
-      subtitle: Text('${todo.dateTime}',
-        style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),),
+      subtitle: Text(
+        DateFormat.yMMMd()
+            .format(DateTime.fromMillisecondsSinceEpoch(todo.dateTime)),
+        style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
+      ),
     );
   }
 }
